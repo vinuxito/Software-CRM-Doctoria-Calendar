d249534eda70bbb76a9ed52f89655c315745be09
Victor Cazares <victor@filemon.net>
2026-07-12 23:29:42 -0600

    fix(core): change require_once to require in Controller::view to allow multiple view renderings

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -17,7 +17,7 @@ class Controller {
     public function view($view, $data = []){
         // Check for view file
         if(file_exists('app/views/' . $view . '.php')){
-            require_once 'app/views/' . $view . '.php';
+            require 'app/views/' . $view . '.php';
         } else {
             // View does not exist
             die('View does not exist');
