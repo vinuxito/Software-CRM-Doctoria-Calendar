2ef47c7309ac5c678a047f30505059aebf266255
Victor Cazares <victor@filemon.net>
2026-07-12 23:20:58 -0600

    fix(auth): change email input to type text to allow username entries

--- app/views/auth/login.php
+++ app/views/auth/login.php
@@ -6,8 +6,8 @@
             <p>Please fill in your credentials to log in</p>
             <form action="<?php echo URLROOT; ?>/users/login" method="post">
                 <div class="form-group mb-3">
-                    <label for="email">Email: <sup>*</sup></label>
-                    <input type="email" name="email" class="form-control form-control-lg <?php echo (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['email']; ?>">
+                    <label for="email">Email o Usuario: <sup>*</sup></label>
+                    <input type="text" name="email" class="form-control form-control-lg <?php echo (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['email']; ?>">
                     <span class="invalid-feedback"><?php echo $data['email_err']; ?></span>
                 </div>
                 <div class="form-group mb-3">
